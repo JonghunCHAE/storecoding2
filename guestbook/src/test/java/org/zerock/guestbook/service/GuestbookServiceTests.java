092794f4071598ec8a7f25b0db0a83cf60cb45ca
chaejonghun <ace1373@naver.com>
2021-01-14 23:30:34 +0900

    210페이지까지완료함

--- guestbook/src/test/java/org/zerock/guestbook/service/GuestbookServiceTests.java
+++ guestbook/src/test/java/org/zerock/guestbook/service/GuestbookServiceTests.java
@@ -26,7 +26,7 @@ public class GuestbookServiceTests {
         System.out.println(service.register(guestbookDTO));
     }
 
-    @Test
+    //@Test
     public void testList(){
 
         PageRequestDTO pageRequestDTO = PageRequestDTO.builder().page(1).size(10).build();
@@ -43,6 +43,31 @@ public class GuestbookServiceTests {
         }
 
         System.out.println("=================================");
+        resultDTO.getPageList().forEach(i -> System.out.println(i));
+    }
+
+    @Test
+    public void testSearch(){
+
+        PageRequestDTO pageRequestDTO = PageRequestDTO.builder()
+                .page(1)
+                .size(10)
+                .type("tc")
+                .keyword("한글")
+                .build();
+
+        PageResultDTO<GuestbookDTO, Guestbook> resultDTO = service.getList(pageRequestDTO);
+
+        System.out.println("PREV: " + resultDTO.isPrev());
+        System.out.println("NEXT: " + resultDTO.isNext());
+        System.out.println("TOTAL: " + resultDTO.getTotalPage());
+
+        System.out.println("------------------------------------------");
+        for(GuestbookDTO guestbookDTO : resultDTO.getDtoList()){
+            System.out.println(guestbookDTO);
+        }
+
+        System.out.println("===========================================");
         resultDTO.getPageList().forEach(i -> System.out.println(i));
     }
 }
