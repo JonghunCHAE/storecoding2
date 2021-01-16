aeda715bddeeea58cf6c50a551fe60ad89270bcd
chaejonghun <ace1373@naver.com>
2021-01-16 13:49:45 +0900

    330페이지까지 완벽

--- board/src/test/java/org/zerock/board/repository/ReplyRepositoryTests.java
+++ board/src/test/java/org/zerock/board/repository/ReplyRepositoryTests.java
@@ -6,6 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.zerock.board.entity.Board;
 import org.zerock.board.entity.Reply;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.stream.IntStream;
 
@@ -34,7 +35,7 @@ public class ReplyRepositoryTests {
         });
     }
 
-    @Test
+    //@Test
     public void readReply1(){
 
         Optional<Reply> result = replyRepository.findById(1L);
@@ -43,5 +44,14 @@ public class ReplyRepositoryTests {
 
         System.out.println(reply);
         System.out.println(reply.getBoard());
+    }
+
+    @Test
+    public void testListByBoard(){
+
+        List<Reply> replyList = replyRepository.getRepliesByBoardOrderByRno(
+                Board.builder().bno(97L).build());
+
+        replyList.forEach(reply -> System.out.println(reply));
     }
 }
