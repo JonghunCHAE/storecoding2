53f7de72e8ec3d0803971404e3f3363a976c3e3c
chaejonghun <ace1373@naver.com>
2021-01-17 22:36:18 +0900

    502페이지 로그인/로그아웃까지 처리완료

---
+++ club/src/main/java/org/zerock/club/config/SecurityConfig.java
@@ -0,0 +1,44 @@
+package org.zerock.club.config;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@Log4j2
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    //새로 추가
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.authorizeRequests()
+                .antMatchers("/sample/all").permitAll()
+                .antMatchers("/sample/member").hasRole("USER");
+
+        http.formLogin();
+        http.csrf().disable();
+        http.logout();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        //사용자 계정은 user1
+        auth.inMemoryAuthentication().withUser("user1")
+        //1111패스워드 인코딩 결과
+        .password("$2a$10$g0KCQ2qGRELr19VUuWCkU.p/eX5O1huX.aM88TG2hL1IUYh1zPXs2")
+        .roles("USER");
+    }
+
+}
