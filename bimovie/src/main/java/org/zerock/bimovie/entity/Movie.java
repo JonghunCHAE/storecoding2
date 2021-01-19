ec209bd3e0bb48af33203f9bec0814f4b0857087
chaejonghun <ace1373@naver.com>
2021-01-19 10:29:42 +0900

    629페이지까지 박살냄

---
+++ bimovie/src/main/java/org/zerock/bimovie/entity/Movie.java
@@ -0,0 +1,34 @@
+package org.zerock.bimovie.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@ToString(exclude = "posterList")
+@Table(name = "tbl_movie")
+public class Movie extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long mno;
+
+    private String title;
+
+    @Builder.Default
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "movie", cascade = CascadeType.ALL)
+    private List<Poster> posterList = new ArrayList<>();
+
+    public void addPoster(Poster poster){
+
+        poster.setIdx(this.posterList.size());
+        poster.setMovie(this);
+        posterList.add(poster);
+    }
+}
